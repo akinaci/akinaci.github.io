8c2cf64cded3081bcc81c62d9d32678393d286fa
akinaci <a-kinaci@kellogg.northwestern.edu>
2016-02-10 02:11:40 -0600

    removed comments from list script

--- todo.js
+++ todo.js
@@ -1,21 +1,21 @@
 // This section of code is how we prepare our code
 // so this it is prepared to react to user events.
 $(document).ready(function() {
-  // $(".btn").on("click", addItem);
-  // $(".link-delete").on("click", deleteItem);
-  // $("ol").on("click", ".link-delete", deleteItem);
+   $(".btn").on("click", addItem);
+   $(".link-delete").on("click", deleteItem);
+   $("ol").on("click", ".link-delete", deleteItem);
 });
 
 // This function prompts the user for a to-do item
 // and then appends it to the existing list of items.
 // It also updates the counter at the top of the screen.
 function addItem() {
-  // text = window.prompt("New To-Do Item");
-  // delete_link = '<a href="#" class="link-delete">(Delete)</a>'
-  // $("ol").append("<li>" + text + " " + delete_link + "</li>");
+   text = window.prompt("New To-Do Item");
+   delete_link = '<a href="#" class="link-delete">(Delete)</a>'
+   $("ol").append("<li>" + text + " " + delete_link + "</li>");
   //
-  // numItems = $("li").length;
-  // $(".total").html(numItems + " items");
+   numItems = $("li").length;
+   $(".total").html(numItems + " items");
 
   // Challenge: Differentiate between 1 item vs. more items
 
@@ -25,7 +25,7 @@ function addItem() {
 // The "event" parameter holds information on which
 // item the user wishes to remove.
 function deleteItem(event) {
-  // console.info(event);
-  // $(event.target).remove();
-  // $(event.target).parent().remove();
+   console.info(event);
+   $(event.target).remove();
+   $(event.target).parent().remove();
 }
